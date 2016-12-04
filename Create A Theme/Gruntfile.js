2c2ba05031f596e7003650077cb97d273764536d
Arjun Kalburgi <askalburgi@gmail.com>
2016-12-04 02:45:31 -0700

    beginning of Gruntfile :D

--- Create A Theme/Gruntfile.js
+++ Create A Theme/Gruntfile.js
@@ -1,4 +1,27 @@
-// Compile scss into css file
+		// Compile scss (source dir) into css file (../Themes dir)
 // Add css file to Themes folder
 // Add css file to Reveal\ Files/css/theme folder 
 // Add link to index.html for presentation view
+
+module.exports = function(grunt) {
+  // Do grunt-related things in here
+  grunt.initConfig({
+  		pkg: grunt.file.readJSON("package.json"),
+
+		sass: {
+			dist: {
+				files: [{
+					expand: true, 
+					cwd: 'source', 
+					src: ['*.scss'], 
+
+					dest: '../Themes', 
+					ext: '.css'
+				}]
+			}
+		}
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.registerTask("default", ['sass']);
+};
